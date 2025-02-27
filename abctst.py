76267698f7f577f2337314027313dda881e36ba4
Matej Holubec <matej.holubec@gmail.com>
2025-02-27 19:25:28 +0900

    chore: comments, fix: replication

--- abctst.py
+++ abctst.py
@@ -1,8 +1,9 @@
 SUFFIX = 'new.py'
 # This is a script that generates a new file with the same content as itself
-print_statements = [r"def main():", r"    print_statements.insert(0, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')",  r"    f = open(__file__ + 'new.py', 'w')", r"    f.write('\n'.join(print_statements))", r"    f.close()", r"    for statement in print_statements:", r"        print(statement)", r"", r"if __name__ == '__main__':", r"    main()"]
+print_statements = [r"SUFFIX = 'new.py'", r"# This is a script that generates a new file with the same content as itself", r"def main():", r"    # Add the first line containing the file line array, use the unicode character to work both here and in the declaration line", r"    print_statements.insert(2, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')",  r"    f = open(__file__ + SUFFIX, 'w')", r"    f.write('\n'.join(print_statements))", r"    f.close()", r"    for statement in print_statements:", r"        print(statement)", r"", r"if __name__ == '__main__':", r"    main()"]
 def main():
-    print_statements.insert(0, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')
+    # Add the first line containing the file line array, use the unicode character to work both here and in the declaration line
+    print_statements.insert(2, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')
     f = open(__file__ + SUFFIX, 'w')
     f.write('\n'.join(print_statements))
     f.close()
