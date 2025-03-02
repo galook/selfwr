fba9978f8708bdf249586247459c98930390df1f
Matej Holubec <matej.holubec@gmail.com>
2025-03-02 14:56:00 +0900

    chore: comments

--- abctst.py
+++ abctst.py
@@ -1,13 +1,14 @@
 SUFFIX = 'new.py'
 # This is a script that generates a new file with the same content as itself
-print_statements = [r"SUFFIX = 'new.py'", r"# This is a script that generates a new file with the same content as itself", r"def main():", r"    # Add the first line containing the file line array, use the unicode character to work both here and in the declaration line", r"    print_statements.insert(2, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')",  r"    f = open(__file__ + SUFFIX, 'w')", r"    f.write('\n'.join(print_statements))", r"    f.close()", r"    for statement in print_statements:", r"        print(statement)", r"", r"if __name__ == '__main__':", r"    main()"]
+print_statements = [r"SUFFIX = 'new.py'", r"# This is a script that generates a new file with the same content as itself", r"def main():", r"    # Add the first line containing the file line array, use the unicode character to work both here and in the declaration line", r"    print_statements.insert(2, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')", r"    # Open a new file and write the content", r"    f = open(__file__ + SUFFIX, 'w', encoding='utf-8')", r"    f.write('\n'.join(print_statements))", r"    f.close()", r"    # Write the content to the console", r"    for statement in print_statements:", r"        print(statement)", r"", r"if __name__ == '__main__':", r"    main()"]
 def main():
     # Add the first line containing the file line array, use the unicode character to work both here and in the declaration line
     print_statements.insert(2, 'print_statements = [r\042' + '\042, r\042'.join(print_statements) + '\042]')
-    
+    # Open a new file and write the content
     f = open(__file__ + SUFFIX, 'w', encoding='utf-8')
     f.write('\n'.join(print_statements))
     f.close()
+    # Write the content to the console
     for statement in print_statements:
         print(statement)
 
